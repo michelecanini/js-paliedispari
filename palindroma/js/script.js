60d767a01b4af4776cea94f5aac8aec96b758356
michelecanini <studio.michelecanini@gmail.com>
2023-05-26 01:53:40 +0200

    [UPDATE] create a function

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,6 +1,13 @@
 "use strict";
 //PROBLEMA: Palindroma, chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma.
 
-//INSERIMENTO DELLA COSTANTE DI INSERIMENTO DELLA PAROLA CON PROMPT
+//CREAZIONE DELLA COSTANTE DI INSERIMENTO DELLA PAROLA CON PROMPT
 const entered_word = prompt("Inserisci una parola e ti diremo se è Palindroma: ");
-console.log(typeof entered_word)
+console.log(entered_word)
+
+//CREAZIONE DELLA FUNZIONE PER LA PAROLA PALINDROMA
+function palindroma_word(entered_word) {
+    
+}
+
+
