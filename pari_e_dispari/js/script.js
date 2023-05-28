7c6404c74d4fedf51e2b643042fcae4ff0f5212f
michelecanini <studio.michelecanini@gmail.com>
2023-05-28 16:11:35 +0200

    [UPDATE] insert prompt

--- pari_e_dispari/js/script.js
+++ pari_e_dispari/js/script.js
@@ -1,2 +1,6 @@
 "use strict";
 //PROBLEMA: L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+// PROMNT DELLA SCELTA UTENTE SE PARI O DISPARI
+let choice = prompt('"Inserire "pari" o "dispari"').toLowerCase();
+console.log(choice)
