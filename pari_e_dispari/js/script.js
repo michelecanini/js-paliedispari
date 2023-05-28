05f67bc1e7fe0fec0bfe7d11e1aa28bcb141d202
michelecanini <studio.michelecanini@gmail.com>
2023-05-28 16:52:42 +0200

    [UPDATE] insert prompt number

--- pari_e_dispari/js/script.js
+++ pari_e_dispari/js/script.js
@@ -3,10 +3,20 @@
 
 // PROMNT DELLA SCELTA UTENTE SE PARI O DISPARI
 let choice = prompt('"Inserire "pari" o "dispari"').toLowerCase();
-console.log(choice)
 
 // CONTROLLO DEL VALORE INSERITO DALL'UTENTE
- if (choice !== "pari" && choice !== "dispari") {
+while (choice !== "pari" && choice !== "dispari") {
     alert('ATTENZIONE! Inserisci un valore valido, scegli tra "pari" o "dispari"');
     choice = prompt('"Inserire "pari" o "dispari"').toLowerCase();
+    console.log(choice)
+ }
+
+// PROMNT DELLA SCELTA UTENTE NUMERO DA 1 A 5
+let insert_number = parseInt(prompt('Inserire un numero compreso tra 1 e 5'));
+
+// CONTROLLO DEL VALORE INSERITO DALL'UTENTE
+while (isNaN(insert_number) || insert_number < 1 || insert_number > 5) {
+    alert('ATTENZIONE! Inserisci un valore valido, scegli un numero compreso tra "1" e "5"');
+    insert_number = prompt('"Inserire un numero compreso tra 1 e 5"').toLowerCase();
+    console.log(insert_number)
  }
